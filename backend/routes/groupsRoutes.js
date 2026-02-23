7d71f635a278a8e6bcd58804adce0322570df49f
BevzoI <bevzo.i.n@gmail.com>
2026-02-23 12:20:52 +0100

    edit routers

--- backend/routes/groupsRoutes.js
+++ backend/routes/groupsRoutes.js
@@ -1,10 +1,12 @@
 import express from "express";
 import { getAllGroups, createGroup } from "../controllers/groupsController.js";
-import { isAdmin } from "../middleware/isAdmin.js";
 
 const router = express.Router();
 
+// GET всі групи
 router.get("/", getAllGroups);
-router.post("/", isAdmin, createGroup);
+
+// POST створити групу (поки без middleware)
+router.post("/", createGroup);
 
 export default router;
