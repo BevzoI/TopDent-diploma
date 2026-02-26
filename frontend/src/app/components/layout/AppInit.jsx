e45df57d30be6a81950ee31a83e2df36f873ea89
BevzoI <bevzo.i.n@gmail.com>
2026-02-26 12:23:09 +0100

    edit appinit

--- frontend/src/app/components/layout/AppInit.jsx
+++ frontend/src/app/components/layout/AppInit.jsx
@@ -1,17 +1,24 @@
 import { useAuthContext } from "../../context/AuthContext";
+import { useLocation } from "react-router-dom";
 
 export default function AppInit({ children }) {
   const { user, loading } = useAuthContext();
+  const location = useLocation();
 
   if (loading) {
     return <div>Načítání...</div>;
   }
 
-  // ❌ Немає авторизації – показуємо тільки goodbye-user
-  if (!user) {
-    return 'Access denied!';
+  const publicRoutes = [
+    "/goodbye-user",
+  ];
+
+  const isInviteRoute = location.pathname.startsWith("/invite");
+
+  // ✅ дозволяємо public маршрути
+  if (!user && !publicRoutes.includes(location.pathname) && !isInviteRoute) {
+    return <div>Access denied!</div>;
   }
 
-  // ✅ Є авторизація – показуємо все, що було всередині <AppInit>...</AppInit>
   return children;
 }
